4ee8daf4c730848669b77a4c94daa64245654e12
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-23 21:48:40 +0300

    updated script

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+  });
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+  app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
